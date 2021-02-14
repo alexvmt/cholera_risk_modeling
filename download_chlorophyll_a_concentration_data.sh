28eaac72862ecb9890e98a27934989d4c1eea6cc
alexvmt <alexandermerdiantarko@gmail.com>
2021-02-14 19:26:19 +0100

    download sss and chlor_a

---
+++ download_chlorophyll_a_concentration_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'Download chlorophyll-a concentration data...'
+if [ ! -d ../data/chlorophyll_a_concentration ]; then
+  mkdir -p ../data/chlorophyll_a_concentration;
+fi
+for year in 2010 2011 2012 2013 2014 2015
+do
+if [ ! -d ../data/chlorophyll_a_concentration/$year ]; then
+  mkdir -p ../data/chlorophyll_a_concentration/$year;
+fi
+wget --mirror --continue --no-host-directories ftp://anon-ftp.ceda.ac.uk/neodc/esacci/ocean_colour/data/v4.2-release/geographic/netcdf/chlor_a/monthly/v4.2/$year --directory-prefix=../data/chlorophyll_a_concentration/$year
+mv ../data/chlorophyll_a_concentration/$year/neodc/esacci/chlorophyll_a_concentration/data/v02.31/30days/$year/* ../data/chlorophyll_a_concentration/$year
+rm -rf ../data/chlorophyll_a_concentration/$year/neodc
+done
+wait
+echo 'Download chlorophyll-a concentration data complete.'
---
+++ download_sea_surface_salinity_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'Download sea surface salinity data...'
+if [ ! -d ../data/sea_surface_salinity ]; then
+  mkdir -p ../data/sea_surface_salinity;
+fi
+for year in 2010 2011 2012 2013 2014 2015
+do
+if [ ! -d ../data/sea_surface_salinity/$year ]; then
+  mkdir -p ../data/sea_surface_salinity/$year;
+fi
+wget --mirror --continue --no-host-directories ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sea_surface_salinity/data/v02.31/30days/$year --directory-prefix=../data/sea_surface_salinity/$year
+mv ../data/sea_surface_salinity/$year/neodc/esacci/sea_surface_salinity/data/v02.31/30days/$year/* ../data/sea_surface_salinity/$year
+rm -rf ../data/sea_surface_salinity/$year/neodc
+done
+wait
+echo 'Download sea surface salinity data complete.'
