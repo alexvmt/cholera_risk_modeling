13337c39e2df3574ff3655e8b938b082da6e7e28
alexvmt <alexandermerdiantarko@gmail.com>
2022-04-10 14:08:03 +0200

    minor edits

--- download_cholera_outbreaks_data.py
+++ download_cholera_outbreaks_data.py
@@ -12,17 +12,13 @@ import requests
 import urllib3
 
 # create data directory if it doesn't yet exist
-if os.path.isdir('../data'):
-    pass
-else:
+if not os.path.exists('../data'):
     os.mkdir('../data')
 
 os.chdir('../data')
 
 # create cholera outbreaks directory if it doesn't yet exist
-if os.path.isdir('cholera_outbreaks'):
-    pass
-else:
+if not os.path.exists('cholera_outbreaks'):
     os.mkdir('cholera_outbreaks')
 
 os.chdir('cholera_outbreaks')
@@ -30,9 +26,7 @@ os.chdir('cholera_outbreaks')
 # create directories for years if they don't yet exist
 years = list(np.arange(2010, 2019))
 for year in years:
-    if os.path.isdir(str(year)):
-        pass
-    else:
+    if not os.path.exists(str(year)):
         os.mkdir(str(year))
 
 # disable insecure request warning
@@ -54,8 +48,11 @@ table = soup.find('table')
 # extract table rows
 rows = table.find_all(lambda tag: tag.name=='tr')
 
+# relevant rows for 2010 to 2018
+relevant_rows = [13, 12, 11, 10, 9, 8, 7, 6, 5]
+
 # download cholera outbreaks pdf files
-for row in range(12, 3, -1):
+for row in relevant_rows:
     
     # get year and number of files in row
     year = rows[row].find('div').text
@@ -71,14 +68,15 @@ for row in range(12, 3, -1):
         # get file and week
         file = rows[row].find_all('a')[file_number].get('href')
         week = rows[row].find_all('a')[file_number].text
-          
+        
         # make file name from week, year and extension
         file_name = week + '_' + year + '.' + ext
-          
+        
         # skip file if it already exists
         if file_name in files_in_directory:
             pass
-        # the links to some files seem to be broken
+            
+        # the links to some files seem to be broken, so these files need to be downloaded manually
         elif file_name == '52nd_2011.pdf':
             r = requests.get('https://www.idsp.nic.in/WriteReadData/DOB2011/52nd_wk11_u.pdf', verify=False)
             with open(year+'/'+file_name, 'wb') as f:
@@ -95,6 +93,7 @@ for row in range(12, 3, -1):
             r = requests.get('https://www.idsp.nic.in/WriteReadData/l892s/15366628751488435411.pdf', verify=False)
             with open(year+'/'+file_name, 'wb') as f:
                 f.write(r.content)
+                
         # download file and save it in the respective directory
         else:
             r = requests.get(file, verify=False)
